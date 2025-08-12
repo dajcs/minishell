65cc1ac98646c9b5a29153c2993df2e5f39a3908
Attila Nemet <anemet@c2r7s5.42luxembourg.lu>
2025-08-12 09:45:45 +0200

    libft norminette fix

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: anemet <anemet@student.42luxembourg.lu>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 13:05:27 by anemet            #+#    #+#             */
-/*   Updated: 2025/08/05 14:40:02 by anemet           ###   ########.fr       */
+/*   Updated: 2025/08/12 09:44:08 by anemet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,6 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	return (0);
 }
 
-
-#include <stddef.h>
-
 int	ft_strcmp(const char *s1, const char *s2)
 {
 	while (*s1 && (*s1 == *s2))
@@ -40,5 +37,5 @@ int	ft_strcmp(const char *s1, const char *s2)
 		s1++;
 		s2++;
 	}
-	return (unsigned char)(*s1) - (unsigned char)(*s2);
+	return ((unsigned char)(*s1) - (unsigned char)(*s2));
 }
